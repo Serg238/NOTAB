801675f3bffebceda8e68f74f983310705709a74
Serg238 <66150886+Serg238@users.noreply.github.com>
2021-12-06 18:09:15 +0600

    Update script.js

--- js-slider/script.js
+++ js-slider/script.js
@@ -1,59 +1,165 @@
-let images = document.querySelectorAll('.infinity-slider img'),
-left_button = document.querySelector('.left_button'),
-right_button = document.querySelector('.right_button'),
-button_three = document.querySelector('.button_three'),
-button_item = document.querySelectorAll('.button_item'),
-current = 0;
+var duration = 360;
+var margin = 960;
+var articles = Array.from(document.getElementsByClassName("slider_item"));
+var animation = false;
+var slide = 0;
+var boxes = Array.from(document.getElementById("controls").children);
 
-slider(current);
-function slider(current) {
-    for (let i = 0; i < images.length; i++) {
-        images[i].classList.add('opacity0');
+function load(){
+    for(let i = 0; i < articles.length; i++){
+        articles[i].style.display = 'none';
+        articles[i].style.zIndex = 0;
     }
+    articles[0].style.zIndex = 10;
+    articles[0].style.display = 'block';
+}
 
-    images[current].classList.remove('opacity0');
+function accur(prevSlide){
+    articles[slide].style.marginLeft = 0;
+    articles[prevSlide].style.zIndex = 0;
+    articles[prevSlide].style.display = "none";
+    animation = false;
     
-   
-    for (let i = 0; i < button_item.length; i++) {
-        button_item[i].classList.remove('new_class');
+}
+
+function animLeftOut(k){
+    var startTime = Date.now();
+    articles[k].style.marginLeft = 0;
+    var timer = setInterval(function(){
+        let timePassed = Date.now() - startTime;
+        if(timePassed >= duration){
+            clearInterval(timer);
+        }
+        draw(timePassed);
+    }, 20);  
+    function draw(timePassed){
+        articles[k].style.marginLeft = -timePassed*margin/duration;               
     }
-
-    button_item[current].classList.add('new_class');
 }
 
-slider(current);
-
-function left() {
-    if(current-1 == -1) current = images.length-1;
-    else current--;
-    slider(current);
-}
-
-left_button.onclick = left;
-
-function right() {
-    if(current+1 == images.length) current = 0;
-    else current++;
-    slider(current);
-}
-
-right_button.onclick = right;
-
-function autoSlider(){
-    slider(current);
-    right();
-}
-
-// setInterval(autoSlider,2000);
-
-function clearSlider(current) {
-    slider(current);
-}
-
-button_three.onclick = function(e) {
-    for (let i = 0; i < button_item.length +1; i++) {
-        if(e.target.classList.contains('button_item') && e.target == button_item[i])
-            clearSlider(i);
-        
+function animLeftIn(k){
+    var startTime = Date.now();
+    var beginMargin = margin;
+    articles[k].style.marginLeft = beginMargin;      
+    articles[k].style.display = 'block';      
+    var timer = setInterval(function(){
+        let timePassed = Date.now() - startTime;
+        if(timePassed >= duration){
+            clearInterval(timer);
+        }
+        draw(timePassed);
+    }, 0);  
+    function draw(timePassed){       
+        articles[k].style.zIndex = 10;
+        articles[k].style.marginLeft = beginMargin - timePassed*margin/duration;  
     }
+}
+
+function animRightOut(k){
+    var startTime = Date.now();
+    articles[k].style.marginLeft = 0;
+    var timer = setInterval(function(){
+        let timePassed = Date.now() - startTime;
+        if(timePassed >= duration){
+            clearInterval(timer);
+        }
+        draw(timePassed);
+    }, 20);  
+    function draw(timePassed){
+        articles[k].style.marginLeft = timePassed*margin/duration;               
+    }
+}
+
+function animRightIn(k){
+    var startTime = Date.now();
+    var beginMargin = -margin;
+    articles[k].style.marginLeft = beginMargin;      
+    articles[k].style.display = 'block';         
+    var timer = setInterval(function(){
+        let timePassed = Date.now() - startTime;
+        if(timePassed >= duration){
+            clearInterval(timer);
+        }
+        draw(timePassed);
+    }, 0);  
+    function draw(timePassed){       
+        articles[k].style.zIndex = 10;
+        articles[k].style.marginLeft = beginMargin + timePassed*margin/duration; 
+    }
+}
+
+function nextPage(){
+    if(!animation){
+        animation = true;
+        var prev = slide;
+        if(slide >= articles.length - 1){
+            setSlide(0);
+        }else{
+            setSlide(slide + 1);
+        }
+        animLeftOut(prev);
+        animLeftIn(slide); 
+        setTimeout(function(){
+            accur(prev);
+        }, duration+20);
+    }
+}
+
+function previousPage(){
+    if(!animation){
+        animation = true;
+        var prev = slide;
+        if(slide <= 0){
+            setSlide(articles.length - 1);
+        }else{
+            setSlide(slide - 1);
+        }
+        animRightOut(prev);
+        animRightIn(slide); 
+        setTimeout(function(){
+            accur(prev);
+        }, duration+20);
+    }
+}
+
+function setPage(n){
+    if(!animation){
+        var prev = slide;
+        animation = true;
+        if(n > articles.length - 1){
+            setSlide(n % articles.length - 1);
+        }else if(n < 0){
+            setSlide(articles.length - 1);
+        }else if(n != slide){
+            setSlide(n);
+        }
+        if(slide > prev){
+            animLeftOut(prev);
+            animLeftIn(slide);
+        }
+        if(slide < prev){
+            animRightOut(prev);
+            animRightIn(slide);
+        }
+        setTimeout(function(){
+            accur(prev);
+        }, duration+20);
+    }
+}
+
+function resetAnimation(){
+    animation = false;
+}
+
+function setSlide(n){
+    boxes[slide].classList.remove("checked");
+    if(n < 0){
+        n = articles.length - 1;
+    }
+    if(n > articles.length - 1){
+        n = 0;
+    }
+    slide = n;
+    boxes[n].classList.add("checked");
+    
 }
